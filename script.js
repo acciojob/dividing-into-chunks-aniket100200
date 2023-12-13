edfcc879477b84d20f09c2bd71813dbdd37d63db
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-13 09:17:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
-const divide = (arr, n) => 
-{
-  // Write your code here
-	
-	let ans=[];
-	let sum=0;
-	let carr=[];
-	for(let i=0;i<arr.length;i++)
-	{
-		if(arr[i]+sum<=n){
-			carr.push(arr[i]);
-			sum+=arr[i];
-		}
-		else
+const divide = (arr, n) => {
+	// Write your code here
+
+	let ans = [];
+	let sum = 0;
+	let carr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (sum+arr[i] == n) 
 		{
 			ans.push(carr);
-			carr=[arr[i]];
-			sum=arr[i];
+			carr=[];
+			sum=0;
+		}
+		else if (arr[i] + sum <= n) {
+			carr.push(arr[i]);
+			sum += arr[i];
+		}
+		else {
+			ans.push(carr);
+			carr = [arr[i]];
+			sum = arr[i];
 		}
 	}
 	ans.push(carr);
